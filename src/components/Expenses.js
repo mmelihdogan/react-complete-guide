4cc565ef264227fd04013fe6fcb35e0734c91029
Melih Doğan <doganmmelih@gmail.com>
2021-07-05 16:27:39 +0300

    New progress

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,38 +1,40 @@
 import ExpenseItem from "./ExpenseItem";
+import Card from "./Card";
+import "./Expenses.css";
 
 function Expenses(props) {
+  //   let fullItems = props.items.map(function (i) {
+  //     <ExpenseItem
+  //       title={i.title}
+  //       date={i.date}
+  //       amount={i.amount}
+  //     />;
+  //   });
 
-//   let fullItems = props.items.map(function (i) {
-//     <ExpenseItem
-//       title={i.title}
-//       date={i.date}
-//       amount={i.amount}
-//     />;
-//   });
-
-  return <>
+  return (
+    <Card className="expenses">
       <ExpenseItem
-      title={props.items[0].title}
-      date={props.items[0].date}
-      amount={props.items[0].amount}
-    />;
-    <ExpenseItem
-      title={props.items[1].title}
-      date={props.items[1].date}
-      amount={props.items[1].amount}
-    />;
-    <ExpenseItem
-      title={props.items[2].title}
-      date={props.items[2].date}
-      amount={props.items[2].amount}
-    />;
-    <ExpenseItem
-      title={props.items[3].title}
-      date={props.items[3].date}
-      amount={props.items[3].amount}
-    />;
-
-  </>;
+        title={props.items[0].title}
+        date={props.items[0].date}
+        amount={props.items[0].amount}
+      />
+      <ExpenseItem
+        title={props.items[1].title}
+        date={props.items[1].date}
+        amount={props.items[1].amount}
+      />
+      <ExpenseItem
+        title={props.items[2].title}
+        date={props.items[2].date}
+        amount={props.items[2].amount}
+      />
+      <ExpenseItem
+        title={props.items[3].title}
+        date={props.items[3].date}
+        amount={props.items[3].amount}
+      />
+    </Card>
+  );
 }
 
 export default Expenses;
